01fd00894bd824bd73d336177289d8adcea85c7a
Shubham Patil <shubhampatil9410@gmail.com>
2021-04-18 20:37:57 +0530

    committing

--- SampleJava.java
+++ SampleJava.java
@@ -1,5 +1,9 @@
 class SampleJava{
 	public static void main(String[] args){
-		System.out.println("First Git program");
+		System.out.println("Is this your first program");
+		if(args[0].equals("yes") || args[0].equals("Yes") || args[0].equals("Y") || args[0].equals("y")){
+			System.out.println("This is first java program for Git hands on");		}else{
+			System.out.println("Welcomeback");
+		}
 	}
 }
